d15a008d4f255e2a330c1db8eba8c5f6d3c44ac0
arnaud <arn.belliard@gmail.com>
2024-09-19 14:38:32 +0200

    Add extern API Spoonacular

--- src/main/java/com/crea/dev6/firstapi/controller/ProductController.java
+++ src/main/java/com/crea/dev6/firstapi/controller/ProductController.java
@@ -1,16 +1,22 @@
 package com.crea.dev6.firstapi.controller;
 
 import java.net.URI;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.crea.dev6.firstapi.dao.ProductDao;
@@ -22,6 +28,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+
 
 @Tag(name = "Product API", description = "GET and POST method for Products")
 @RestController
@@ -109,7 +117,7 @@ public class ProductController {
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {
                     @Content(schema = @Schema()) })
     })
-    @GetMapping("/productPrice/{price}")
+    @GetMapping("/productPrice/{id}")
     public List<Product> findByPriceSup(@PathVariable("price") double price) {
         return productDao.findByPriceGreaterThan(price);
     }
@@ -130,5 +138,23 @@ public class ProductController {
 
         return ResponseEntity.created(location).build();
     }
+
+    @GetMapping("/getRecipeByIngredient/{id}")
+    public String getRecipeByIngredient(@PathVariable("id") int id) {
+        Optional<Product> product = productDao.findById(id);
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("apiKey", "LH484ulQj4Me5QxQwND8gTANyv7GIyFe");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+        ResponseEntity<Object> reponse = 
+        restTemplate.exchange("https://api.apilayer.com/spoonacular/recipes/complexSearch?query=" + product.get().getName(),
+        HttpMethod.GET, new HttpEntity<>(headers), Object.class);
+
+        return reponse.getBody().toString();
+    }
+    
 
 }
